05392a0734b1a2cf0d765718cbc2a7ff39dfb8d2
Shumimran <164326671+Shumimran@users.noreply.github.com>
2024-06-03 11:47:41 +0500

    Add files via upload
    Aouther: shumaila imran

---
+++ main.ts
@@ -0,0 +1,14 @@
+// Pulling Apart a Nested Object: Imagine you have a list inside another list that shows what a computer 
+//programmer knows, like coding languages, tools, and software frameworks. Find a way to get three specific 
+//skills from this list and show them.
+
+let developerSkills = {
+    Languages: ["javascript", "typescript", "python"],
+    Framework:["React", "Angular", "Vue"],
+    Tools: ["Git","Webpack", "Docker"] 
+};
+
+let{Languages,Framework,Tools}= developerSkills;
+
+console.log ( `Languages: ${Languages}, Framework: ${Framework}, Tools: ${Tools}`);
+
